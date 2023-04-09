9db4144abfd40cc00f6abf1dc6fcb8e26a54e58a
BetoCarrillo <alberto.carrillo01@gmail.com>
2023-04-09 19:48:48 +0200

    initial commit

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Main() {
+  // AQUÍ FUNCTIONS (ejemplo query)
+  return (
+    <div>
+      {/* AQUÍ DISPLAY  (ejemplo crear tabla)*/}
+      <h1>Monday Project</h1>
+    </div>
+  );
+}
+
+export default Main;
